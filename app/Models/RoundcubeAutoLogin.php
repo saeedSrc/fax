142c47b484c10137368cb01d46d4202981529a4f
saeed <saeedrasooli421@gamil.com>
2021-04-02 02:32:42 +0430

    data ware house pvp game

--- app/Models/RoundcubeAutoLogin.php
+++ app/Models/RoundcubeAutoLogin.php
@@ -134,28 +134,28 @@ class RoundcubeAutoLogin
 
             $response = curl_exec($this->ch);
             $response_info = curl_getinfo($this->ch);
+dd($response_info);
+            if($response_info['http_code'] == 302)
+            {
+                // find all relevant cookies to set (php session + rc auth cookie)
+                preg_match_all('/Set-Cookie: (.*)\b/', $response, $cookies);
 
-//            if($response_info['http_code'] == 302)
-//            {
-//                // find all relevant cookies to set (php session + rc auth cookie)
-//                preg_match_all('/Set-Cookie: (.*)\b/', $response, $cookies);
-//
-//                $cookie_return = array();
-//
-//                foreach($cookies[1] as $cookie)
-//                {
-//                    preg_match('|([A-z0-9\_]*)=([A-z0-9\_\-]*);|', $cookie, $cookie_match);
-//                    if($cookie_match) {
-//                        $cookie_return[$cookie_match[1]] = $cookie_match[2];
-//                    }
-//                }
-//
-//                return $cookie_return;
-//            }
-//            else
-//            {
-//                throw new RoundCubeException('خروج شما با خطا مواجه شد لطفا مجددا تلاش کنید.');
-//            }
+                $cookie_return = array();
+
+                foreach($cookies[1] as $cookie)
+                {
+                    preg_match('|([A-z0-9\_]*)=([A-z0-9\_\-]*);|', $cookie, $cookie_match);
+                    if($cookie_match) {
+                        $cookie_return[$cookie_match[1]] = $cookie_match[2];
+                    }
+                }
+
+                return $cookie_return;
+            }
+            else
+            {
+                throw new RoundCubeException('خروج شما با خطا مواجه شد لطفا مجددا تلاش کنید.');
+            }
 
         }
         catch(RoundCubeException $e)
